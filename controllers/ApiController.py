ea9ae533e95198ce395086d7c8659d8fb95e5531
Ilechko Serhii <otakuvibri@gmail.com>
2025-01-23 12:56:43 +0000

    1/3

--- controllers/ApiController.py
+++ controllers/ApiController.py
@@ -1,33 +1,34 @@
 import json
-
+from models.RestModel import *
 class ApiController:
+
+    def __init__(self):
+        self.response = RestModel()
+
+        
     def end_with(self, status_code: int=200, data: any= None):
         if status_code != 200:
-            print(f"Status: {status_code} {self.phrase_by_code(status_code)}")
-        if data != None:
-            print("Content-Type: text/plain; charset=utf-8")
-            print()
-            print(json.dumps(data))
-            exit()
-        else:
-            print("Content-Length: 0")
-            print()
+            self.response.status = RestStatus(status_code)
+            print(f"Status: {status_code} {self.response.status.reasonPhrase}")
+        self.response.data = data
+        print("Content-Type: application/json; charset=utf-8")
+        print()
+        print(json.dumps(dict(self.response.__dict__())))
         exit()
 
 
-    def phrase_by_code(self, status_code: int) -> str:
-        match status_code:
-            case 200: return "OK"
-            case 405: return "Methond Not Allowed"
-            case 415: return "Unsupported Media Type"
-            case _: return ""
-    
-
     def serve(self, access_manager_data):
         self.access_manager_data = access_manager_data
-        action_name = f"do_{access_manager_data["envs"]["REQUEST_METHOD"].lower()}"
+        method = access_manager_data["envs"]["REQUEST_METHOD"]
+
+        self.response.meta = RestMeta({
+            'service': 'Server Application',
+            'group': 'KN-P-213',
+            'method': method
+        })
+        action_name = f"do_{method.lower()}"
         controller_action = getattr( self, action_name, None)
         if controller_action == None:
-            self.end_with(405, f"Method {access_manager_data["envs"]["REQUEST_METHOD"]} not supported in requested endpoint")
+            self.end_with(405, f"Method {method} not supported in requested endpoint")
         else:
             self.end_with(data =controller_action())
